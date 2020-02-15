10dbf93529ecb10c14dd7b99124f41a55ed8556a
Anthony <cdistc@163.com>
2020-02-15 21:30:25 +0800

    gradle script

--- biz-user/src/main/java/com/shit_code/cloud/user/service/impl/UserServiceImpl.java
+++ biz-user/src/main/java/com/shit_code/cloud/user/service/impl/UserServiceImpl.java
@@ -2,7 +2,6 @@ package com.shit_code.cloud.user.service.impl;
 
 import com.shit_code.cloud.user.dto.UserInfoDTO;
 import com.shit_code.cloud.user.service.UserService;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
