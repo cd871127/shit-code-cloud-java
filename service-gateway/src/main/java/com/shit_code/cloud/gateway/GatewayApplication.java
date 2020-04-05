11c952b1f9676c82d1b41e2e9751d261092264a1
Anthony <cdistc@163.com>
2020-04-05 13:42:25 +0800

    config

--- service-gateway/src/main/java/com/shit_code/cloud/gateway/GatewayApplication.java
+++ service-gateway/src/main/java/com/shit_code/cloud/gateway/GatewayApplication.java
@@ -2,10 +2,12 @@ package com.shit_code.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.gateway.route.RouteDefinitionRouteLocator;
 
 @SpringCloudApplication
 public class GatewayApplication {
     public static void main(String[] args) {
+//        https://www.jianshu.com/p/8f007bcf36ea
         SpringApplication.run(GatewayApplication.class, args);
     }
 }
