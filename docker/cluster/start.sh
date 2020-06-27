6206c5ae3465d64ace027875b2268e9c75f5ab01
Anthony <cdistc@163.com>
2020-06-28 02:10:43 +0800

    docker

--- docker/cluster/start.sh
+++ docker/cluster/start.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/env bash
+docker-compose -f compose/01_network.yml -f compose/05_rabbitMq.yml up
+#docker-compose -f compose/01_network.yml -f compose/04_redis.yml up
+#docker-compose -f compose/01_network.yml -f compose/03_mysql.yml up
